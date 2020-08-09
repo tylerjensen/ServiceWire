1adcf56767d37a16721a098322f890c905ec8517
Kenneth Carter <kccarter32@gmail.com>
2020-08-08 19:04:51 -0600

    added the asynchrounous test

--- src/Tests/Unit/ServiceWireTests/TypeMapperTests.cs
+++ src/Tests/Unit/ServiceWireTests/TypeMapperTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ServiceWireTests
@@ -14,6 +15,18 @@ namespace ServiceWireTests
             Type uriType = typeof(Uri);
 
             Type mapType = TypeMapper.GetType(uriType.FullName);
+
+            Assert.NotNull(mapType);
+
+            Assert.Equal(uriType.FullName, mapType.FullName);
+        }
+
+        [Fact]
+        public async Task CanMapUriTypesAsync()
+        {
+            Type uriType = typeof(Uri);
+
+            Type mapType = await TypeMapper.GetTypeAsync(uriType.FullName);
 
             Assert.NotNull(mapType);
 
